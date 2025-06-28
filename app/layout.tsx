14005aaa8e2dcff55081d1a8a8cbc806a67ee82a
ikhodabande <amimhmdkh@gmail.com>
2025-06-28 12:23:46 +0330

    update the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          inter.variable,
-          vazirmatn.variable,
-          notoSansFarsi.variable,
+          // inter.variable,
+          // vazirmatn.variable,
+          // notoSansFarsi.variable,
           "font-sans antialiased transition-all duration-300",
         )}
       >
